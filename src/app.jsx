50ccf0175262269f9b138b75da9fbe8b58ead3e5
amitnitjsr <amitnips007@gmail.com>
2021-07-17 22:32:32 +0530

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Test from '../src/component/Test';
+
+const content = 'Hello world!';
+
+const App = () => (
+  <Fragment>
+    <Test />
+  </Fragment>
+);
+
+export default App;
---
+++ src/component/Test.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Test.scss';
+
+export default class Test extends Component {
+    render() {
+        return (
+            <div className="center">
+                <h1 >Webpack Configuration...</h1>
+            </div>
+        )
+    }
+}
